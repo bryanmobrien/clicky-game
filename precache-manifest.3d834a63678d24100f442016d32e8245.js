58beb21b8580a805cfc969d556d51cdfb28a9f75
Bryan O'Brien <bryan.m.obrien@gmail.com>
2019-10-23 23:20:08 -0500

    Updates

---
+++ precache-manifest.3d834a63678d24100f442016d32e8245.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6643e3f9d4d6dd9772b7780cc261be60",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "0c7265eb00cceda88fd8",
+    "url": "/clicky-game/static/css/main.1768057c.chunk.css"
+  },
+  {
+    "revision": "0bfc807b6e59d15066b3",
+    "url": "/clicky-game/static/js/2.b75dfd5e.chunk.js"
+  },
+  {
+    "revision": "0c7265eb00cceda88fd8",
+    "url": "/clicky-game/static/js/main.4c997c9e.chunk.js"
+  },
+  {
+    "revision": "9e63f44ec9a3dfc4c648",
+    "url": "/clicky-game/static/js/runtime-main.742e30a5.js"
+  }
+]);
